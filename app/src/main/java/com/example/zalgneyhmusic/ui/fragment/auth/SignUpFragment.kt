cf914c852b3f23cc6e220baf30afef058c512122
dtminh <minhm5993@gmail.com>
2025-09-15 21:44:14 +0700

    Fix comment

--- app/src/main/java/com/example/zalgneyhmusic/ui/fragment/auth/SignUpFragment.kt
+++ app/src/main/java/com/example/zalgneyhmusic/ui/fragment/auth/SignUpFragment.kt
@@ -1,10 +1,12 @@
-package com.example.zalgneyhmusic
+package com.example.zalgneyhmusic.ui.fragment.auth
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.zalgneyhmusic.R
+
 class SignUpFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
